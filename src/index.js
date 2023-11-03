4fa74add73d5d0187e30ac818837aa8524250f8d
George <georgejoj28@gmail.com>
2023-11-03 15:32:04 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter} from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
+ReactDOM.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
 
-ReactDOM.render(<App />, document.getElementById("root"));
+, document.getElementById("root"));
